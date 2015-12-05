1fcd4cf9ab5b2b5422fecd4690c939f49003386f
David Roberts <email": null>
2015-12-05 12:40:13 -0500

    get controller and view working until post guess route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,18 +17,21 @@ end
 
 get '/rounds/:id' do
   #This is where the error is!!! in Card.params
-  if Card.params[:id].guesses.all.count >0
-    already_guessed=Card.params[:id].guesses.all.count
+  guesses_from_this_round = Guess.where(round_id: params[:id])
+  cards_in_this_deck = Deck.find_by(id: Round.find_by(id: params[:id]).deck_id).cards
+
+  if guesses_from_this_round.length > 0
+    already_guessed = []
+    guesses_from_this_round.each {|guess| already_guessed << Card.find_by(id: guess.card_id)}
   end
 
-  cards_held=Card.params[:id].guesses.all
+  cards_held=cards_in_this_deck
 
-  if already_guessed.is_defined?
+  if already_guessed
     cards_held.delete_if{|card| already_guessed.include?(card)}
   end
 
   @round_id=params[:id]
-
   @current_card=cards_held.sample
 
   erb :guess_form
